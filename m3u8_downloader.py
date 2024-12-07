02a1abd9e0bfacc830a6f7f5fd587c068b7fbbb7
Hugo Messias <hugomessias9@gmail.com>
2024-12-07 16:08:22 -0300

    Implemented methods to save or load json files containing PartInfo objects - Removed PartInfo.__dict__(), as it was being wrongly used - Implemented load_json() and save_json() as class methods of PartInfo - The constructor of M3U8Downloader now interacts with a json file when reading os saving information about parts, which will later allow it to keep record a of what has already been downloaded between multiple sessions

--- m3u8_downloader.py
+++ m3u8_downloader.py
@@ -1,20 +1,51 @@
 import hashlib
+import json
 import os
 import re
 
 class PartInfo():
-    def __init__(self):
-        self.url = None
-        self.duration = None
-        self.range = None
-        self.index = None
+    def __init__(self, downloaded: bool = False, url: str = None, duration: float = None, range: str = None, index: str = None):
+        self.downloaded = downloaded
+        self.url = url
+        self.duration = duration
+        self.range = range
+        self.index = index
     
     def __repr__(self):
         return f'url: {self.url}, duration: {self.duration}, range: {self.range}, index: {self.index}'
     
-    def __dict__(self):
-        return {'url': self.url, 'duration': self.duration, 'range': self.range, 'index': self.index}
+    # saves a json file of a serializable that contains a PartInfo object
+    @classmethod
+    def save_json(cls, obj, path):
+        def new_default(obj):
+            if isinstance(obj, PartInfo):
+                return obj.__dict__
 
+            else:
+                return obj
+            
+        with open(path, 'w', encoding='utf8') as json_file:
+            json_file.write(json.dumps(obj, indent=2, default=new_default))
+    
+    # loads as json file that contains instances of PartInfo
+    @classmethod
+    def load_json(cls, path):
+        def new_object_hook(obj):
+            if isinstance(obj, dict):
+                part_info_dict = PartInfo().__dict__
+                for key in obj.keys():
+                    if key not in part_info_dict.keys():
+                        return obj
+                
+                return PartInfo(**obj)
+
+            else:
+                return obj
+            
+        with open(path, 'r', encoding='utf8') as json_file:
+            return json.loads(json_file.read(), object_hook=new_object_hook)
+    
+    
 # TODO: progress property
 class M3U8Downloader():
     def __init__(
@@ -27,30 +58,41 @@ class M3U8Downloader():
             max_downloads: int = 5, 
             max_retries: int = 5
         ):
-        self.download_dir = './temp_m3u8_downloader/'
 
-        # necessary args
+        # general attributes
         self.m3u8_path = m3u8_path
         self.output_file = os.path.abspath(output_file)
         self.file_extension = os.path.basename(output_file).rsplit('.')[-1]
+        self.download_dir = './temp_m3u8_downloader/'
+        if headers is None: self.headers = {}
 
-        # optional args
-        if headers is None:
-            self.headers = {}
-
-        if label == '':
-            self.label = os.path.basename(output_file)
-
+        # creates a temporary directory based on the path of the output file
         if temp_dir == '':
             output_hash = hashlib.sha256(self.output_file.encode())
             output_hash = output_hash.hexdigest()
             self.temp_dir = f'{self.download_dir}{output_hash}/'
 
-        # other attributes
-        self.parts = self._extract_info()
+        os.makedirs(self.temp_dir, exist_ok=True)
+
+        # assigns a name to be displayed along with download progress
+        if label == '':
+            self.label = os.path.basename(output_file)
+        
+        # get info from the m3u8, if this file has already been read before, reuse the previous version
+        # to keep track of what has already been downloaded
+        self.parts_json_path = f'{self.temp_dir}/parts.json'
+        if os.path.exists(self.parts_json_path):
+            self.parts = PartInfo.load_json(self.parts_json_path)
+        
+        else:
+            self.parts = self._extract_info()
+            PartInfo.save_json(self.parts, self.parts_json_path)
+            
+        # attributes used for tracking progress   
         self.total_parts = len(self.parts) - 1
         self.curr_part = 0
     
+
     def _extract_info(self) -> list[PartInfo]:
         with open(self.m3u8_path) as playlist:
             lines = playlist.readlines()
@@ -93,7 +135,6 @@ class M3U8Downloader():
                         part_index += 1
                     
                     # TODO: add support for skip and key tags
-
 
         return parts_info
 
