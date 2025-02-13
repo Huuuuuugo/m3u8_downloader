dd3f036c837c671a2e04948c99840bac1ea84937
Hugo Messias <hugomessias9@gmail.com>
2025-02-13 15:16:32 -0300

    Fix headers not being used inside `downloader.py`

--- downloader.py
+++ downloader.py
@@ -6,7 +6,7 @@ import os
 import requests
 
 
-class Download():  
+class Download():
     def __init__(
             self, 
             url: str, 
@@ -31,7 +31,6 @@ class Download():
         self.try_continue = try_continue
         if except_status_codes is None:
             self.except_status_codes = [404, 403, 400, 405, 408, 410, 411, 412, 415, 429, 500, 501, 502, 503, 504]
-
         else:
             self.except_status_codes = except_status_codes
 
@@ -49,7 +48,7 @@ class Download():
             
             # set range to resume download if any byte has already been written
             if self.written_bytes:
-                headers.update({"Range": f"bytes={self.written_bytes}-"})
+                self.headers.update({"Range": f"bytes={self.written_bytes}-"})
         
         else:
             self.total_size = 0
